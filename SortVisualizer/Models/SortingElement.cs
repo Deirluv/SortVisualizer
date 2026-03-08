9eb0c007886f6e47f977c94b8fc78db7f3792ae3
Zuba_ji34@student.itstep.org <Zuba_ji34@student.itstep.org>
2026-03-08 14:48:21 +0100

    Initial commit

---
+++ SortVisualizer/Models/SortingElement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SortVisualizer.Models;
+
+public enum Status
+{
+    NotSorted, InProgress, Sorted
+}
+
+public class SortingElement : INotifyPropertyChanged
+{
+    private int _value;
+    private string _color = "Blue";
+    private Status _status = Status.NotSorted;
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public int Value
+    {
+        get => _value;
+        set => SetField(ref _value, value);
+    }
+    
+    public string Color
+    {
+        get => _color;
+        set => SetField(ref _color, value);
+    }
+
+    public Status Status
+    {
+        get => _status;
+        set => SetField(ref _status, value);
+    }
+    
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
---
+++ SortVisualizer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using SortVisualizer.Models;
+
+namespace SortVisualizer.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public ObservableCollection<SortingElement> SortingElements { get; }
+
+    public MainWindowViewModel()
+    {
+        
+    }
+}
